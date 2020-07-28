3fc992088d0c1e54816d614361ffc5d12a04456f
Roman Sydoruk <sydoruk89@gmail.com>
2020-07-27 22:58:12 -0700

    breadth-first

--- challenges/tree/tree.py
+++ challenges/tree/tree.py
@@ -1,3 +1,80 @@
+class Node_q:
+    """
+    Node class initilizing
+    """
+    def __init__(self, value, next=None):
+        """
+        Init node instance with value and next element
+        """
+        self.value = value
+        self.next = next
+
+    def __str__(self):
+        """ Return an object instance
+        """
+        return f'{self.value} -> {self.next}'
+
+    def __repr__(self):
+        """
+        Return an object instance
+        """
+        return f'{self.value}'
+
+
+class Queue:
+    """class Queue"""
+
+    def __init__(self, next=None):
+        """Initiate class"""
+
+        self.front = None
+        self.rear = None
+
+    def __str__(self) -> str:
+        """Informal string representation
+        """
+        output = []
+        curr = self.front
+        while curr is not None:
+            output.append(curr.value)
+            curr = curr.next
+        return " -> ".join(output)
+
+    def is_empty(self):
+        """method to check if Queue is empty"""
+
+        if self.front is None:
+            return True
+        return False
+
+    def enqueue(self, val):
+        """Method that adds an element to the end of the queue"""
+
+        if self.is_empty():
+            self.front = self.rear = Node_q(val)
+        else:
+            new_node = Node(val)
+            self.rear.next, self.rear = new_node, new_node
+
+    def dequeue(self):
+        """Method that removes the node from the front of the queue, and returns its value."""
+
+        if not self.is_empty():
+            temp = self.front
+            self.front = self.front.next
+            temp.next = None
+            return temp
+        else:
+            raise Exception('Queue is empty!')
+
+    def peek(self):
+        """Method returns the value of the front node"""
+
+        if not self.is_empty():
+            return self.front.value
+        return None
+
+
 class Node:
     """
     Node class which instantiates node value with right or left direction
@@ -13,8 +90,8 @@ class BinaryTree:
     """
     BinaryTree class instantiation with pre_order, in_order and post_order methods.
     """
-    def __init__(self):
-        self.root = None
+    def __init__(self, root):
+        self.root = Node(root)
 
     def pre_order(self):
         """
@@ -76,13 +153,29 @@ class BinaryTree:
 
     def find_maximum_value(self):
         """
-        This method returns max value of the BST
+        This method returns the max value of the BST
         """
         current = self.root
         while current.right:
             current = current.right
         return current.value
-
+    
+    def breadth_first(self):
+        """
+        This method returns a list of the values in the tree in the order they were encountered
+        """
+        breadth = Queue()
+        breadth.enqueue(self.root)
+        output = []
+        while breadth.peek():
+            output.append(breadth.peek())
+            node = breadth.dequeue()
+            if node.left:
+                breadth.enqueue(node.left)
+            if node.right:
+                breadth.enqueue(node.right)
+        return output
+ 
 
 class BST(BinaryTree):
     """
@@ -138,12 +231,11 @@ class BST(BinaryTree):
         traverse(self.root, key)
 
         
-bst = BST()
-bst.root = Node(7)
+bst = BST(7)
 bst.add(4)
 bst.add(3)
 bst.add(2)
 bst.add(5)
 bst.add(9)
 bst.add(11)
-print(bst.find_maximum_value())
+print(bst.breadth_first())
